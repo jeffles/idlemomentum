fa619c2c815502a803432b3481e346284443f2da
Jeff Sumner <jeff.sumner@gmail.com>
2020-09-12 15:46:30 -0500

    Added file compare - reset when no progress
    Ran the numbers running for a full hour 3 times, and the local maximum appears to be at the major reset when progress then stops until the minors catch up. Changing so ascends happen when progress on APs has halted for 3 minutes. Also shrank file size to just the AP so I can do OCR next instead of file compare.

--- momentum.py
+++ momentum.py
@@ -4,6 +4,7 @@ import time
 import sys
 import subprocess
 
+from filecmp import cmp
 from twisted.internet import task, reactor
 
 mytimes = {
@@ -13,29 +14,43 @@ mytimes = {
 
 ascension = 0
 pyautogui.PAUSE=0.4
-startTime = time.time()
+start_time = time.time()
+previous_file_name = ''
+run_without_improvement = 0
 
 def get_print_time():
-    minute = str(math.ceil((time.time()-startTime)/60))
+    minute = str(math.ceil((time.time()-start_time)/60))
     if (ascension > 0):
         return minute + '-' + str(ascension)
     return minute
 
-def get_new_end_time(minutes=15):
-    return time.time() + 60 * minutes
-
 def ascend():
+    global ascension
+    global start_time
     print('Acending at', get_print_time())
     ascension+=1
+    start_time = time.time()
     pyautogui.moveTo(431, 138)
     pyautogui.dragTo()
     pyautogui.click()
     #pyautogui.click(x=431, y=138, clicks=2, interval=1)
 
 def screen_shot():
+    global previous_file_name
+    global run_without_improvement
     print('Screen shot at', get_print_time())
     file_name = 'ascend' + get_print_time() + '.png'
-    pyautogui.screenshot(file_name, region=(690,235,310,70))
+    pyautogui.screenshot(file_name, region=(865,235,160,70))
+    if previous_file_name:
+        same = cmp(previous_file_name, file_name)
+        if same:
+            run_without_improvement += 1
+        else:
+            run_without_improvement = 0
+    previous_file_name = file_name
+    if start_time > 300 and run_without_improvement > 1:
+        ascend();
+
        
 def level_up():
     pyautogui.press('space')
@@ -59,7 +74,7 @@ loop1.start(.5)
 loop2 = task.LoopingCall(screen_shot) 
 loop2.start(mytimes['screen_shot']*60, now=True)
 
-loop3 = task.LoopingCall(ascend)
-loop3.start(mytimes['ascend']*60, now=False)
+# loop3 = task.LoopingCall(ascend)
+# loop3.start(mytimes['ascend']*60, now=False)
 
 reactor.run()
