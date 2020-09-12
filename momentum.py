54e5dc55f3513e3c887b3ac9308d95f56a35ff9b
Jeff Sumner <jeff.sumner@gmail.com>
2020-09-12 11:10:21 -0500

    Add files via upload

---
+++ momentum.py
@@ -0,0 +1,60 @@
+import math
+import pyautogui
+import time
+import sys
+import subprocess
+
+from twisted.internet import task, reactor
+
+mytimes = {
+	"screen_shot" = 1
+	"ascend" = 60
+}
+
+pyautogui.PAUSE=0.4
+startTime = time.time()
+
+def get_print_time():
+	return str(math.ceil((time.time()-startTime)/60))
+
+def get_new_end_time(minutes=15):
+	return time.time() + 60 * minutes
+
+def ascend():
+	print('Acending at', get_print_time())
+	pyautogui.moveTo(431, 138)
+	pyautogui.dragTo()
+	pyautogui.click()
+	#pyautogui.click(x=431, y=138, clicks=2, interval=1)
+
+def screen_shot():
+	print('Screen shot at', get_print_time())
+	file_name = 'ascend' + get_print_time() + '.png'
+	pyautogui.screenshot(file_name, region=(0,0,1200,800))
+       
+def level_up():
+	pyautogui.press('space')
+
+
+print (sys.platform)
+if 'darwin' in sys.platform:
+    print('Running \'caffeinate\' on MacOSX to prevent the system from sleeping')
+    subprocess.Popen('caffeinate')
+# while (True):
+# 	currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
+# 	print (currentMouseX, currentMouseY)
+pyautogui.moveTo(1393, 138) 
+pyautogui.dragTo()
+pyautogui.click()          
+pyautogui.click() 
+
+loop1 = task.LoopingCall(level_up)
+loop1.start(.5)   
+
+loop2 = task.LoopingCall(screen_shot) 
+loop2.start(mytimes['screen_shot']*60, now=True)
+
+loop3 = task.LoopingCall(ascend)
+loop3.start(mytimes['ascend']*60, now=False)
+
+reactor.run()
